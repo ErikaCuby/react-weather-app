e1d924e0c3f24b70f8df659e17c227ae3bce94b0
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-04-17 13:12:16 +0200

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">React Weather App</div>
+    </div>
+  );
 }
